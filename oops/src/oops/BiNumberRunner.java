e78fa588e1b6addc8f78e68e954ee43c757c4b75
vinod salunke <salunkevinod45@gmail.com>
2026-01-25 14:20:50 +0530

    BiNumber

---
+++ oops/src/oops/BiNumberRunner.java
@@ -0,0 +1,18 @@
+package oops;
+
+public class BiNumberRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BiNumber numbers = new BiNumber(2, 3);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+
+		numbers.doubleNum();
+
+		System.out.println(numbers.getNumber1());
+		System.out.println(numbers.getNumber2());
+
+	}
+
+}
